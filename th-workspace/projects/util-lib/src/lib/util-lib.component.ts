e72613d10a4866bac1dd2c1b5e949d06cd1e41c5
testhero1 <testhero1@users.noreply.github.com>
2019-12-23 14:47:46 -0500

    Added new worksplace, sampleapp1 and util library

---
+++ th-workspace/projects/util-lib/src/lib/util-lib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-util-lib',
+  template: `
+    <p>
+      util-lib works!
+    </p>
+  `,
+  styles: []
+})
+export class UtilLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ th-workspace/projects/util-lib/src/lib/util-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { UtilLibComponent } from './util-lib.component';
+
+
+
+@NgModule({
+  declarations: [UtilLibComponent],
+  imports: [
+  ],
+  exports: [UtilLibComponent]
+})
+export class UtilLibModule { }
